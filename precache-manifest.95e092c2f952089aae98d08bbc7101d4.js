d31336f8f7976a04a8d7c46c42a000a330ebd54f
Ondřej Bazala <ondra.bazala@seznam.cz>
2020-05-21 17:36:09 +0200

    test

---
+++ precache-manifest.95e092c2f952089aae98d08bbc7101d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c6e3417eab8e8cd2bf620055dcbeb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1632979a2d50e648b980",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec7758de3cd29d666a06",
+    "url": "/static/js/2.7a8e3da2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a8e3da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1632979a2d50e648b980",
+    "url": "/static/js/main.456d770e.chunk.js"
+  },
+  {
+    "revision": "a343cbc1110e1c69a4ff",
+    "url": "/static/js/runtime-main.5e784524.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
